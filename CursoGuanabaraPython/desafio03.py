e7d702e3620bfaf44174c566025719a808072834
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-02-08 19:18:43 -0300

    adicionadoDesafiosJaFeito
    Adicionado desafios já feito.

---
+++ CursoGuanabaraPython/desafio03.py
@@ -0,0 +1,4 @@
+print ('===============DESAFIO 03===============')
+num1 = input ('Digite o primeiro número: ')
+num2 = input ('Digite o segundo número: ')
+print ('A soma é', int (num1) + int (num2))
