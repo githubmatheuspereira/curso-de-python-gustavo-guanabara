6e69001a2dfd63c3a0393c22fb595d6c794bc368
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-02-08 21:26:58 -0300

    atualizado desafio03
    incluído a variável "soma"

--- CursoGuanabaraPython/desafio03.py
+++ CursoGuanabaraPython/desafio03.py
@@ -1,4 +1,5 @@
 print ('===============DESAFIO 03===============')
 num1 = input ('Digite o primeiro número: ')
 num2 = input ('Digite o segundo número: ')
-print ('A soma é', int (num1) + int (num2))
+soma = int (num1) + int (num2)
+print ('A soma é', soma)
