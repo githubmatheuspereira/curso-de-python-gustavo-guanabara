64dc457d83af5fbd6d5db5d3e2abb30890a29c90
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-02-14 22:03:04 -0300

    atualizado desafio

--- CursoGuanabaraPython/desafio17.py
+++ CursoGuanabaraPython/desafio17.py
@@ -1,5 +1,10 @@
-from math import sqrt, trunc
-catetoOposto = int(input('Qual o valor do cateto oposto? '))
-catetoAdjacente = int(input('Qual o valor do cateto adjacente? '))
-hipotenusa = int((catetoOposto**2 + catetoAdjacente**2))
-print('Cateto oposto: {}\nCateto adjacente: {}\nHipotenusa: {}'.format(catetoOposto, catetoAdjacente, trunc(sqrt(hipotenusa))))
+from math import sqrt, hypot
+
+catetoOposto = float(input('Qual o valor do cateto oposto? '))
+catetoAdjacente = float(input('Qual o valor do cateto adjacente? '))
+'''
+hipotenusa = ((catetoOposto**2 + catetoAdjacente**2))
+print('Cateto oposto: {}\nCateto adjacente: {}\nHipotenusa: {:.2f}'.format(catetoOposto, catetoAdjacente, sqrt(hipotenusa)))
+'''
+hipotenusa = hypot(catetoOposto, catetoAdjacente)
+print('Cateto oposto: {}\nCateto adjacente: {}\nHipotenusa: {:.2f}'.format(catetoOposto, catetoAdjacente, hipotenusa))
