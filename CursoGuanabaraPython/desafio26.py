284a4aecf4c825bd16ae46dd049e75be81524ed6
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-02-22 16:51:35 -0300

    Incluido função rfind()

--- CursoGuanabaraPython/desafio26.py
+++ CursoGuanabaraPython/desafio26.py
@@ -1,3 +1,3 @@
-frase = str(input('Escreva sua frase abaixo.\n'))
-print('Seu texto foi:\n'.format(frase))
-print('A letra "''A''" aparece {} vezes\nA letra "''A''" apareceu a 1ª vez na memória {}'.format(frase.count('a'), frase.find('a')))
+frase = str(input('Escreva sua frase abaixo.\n')).strip().upper()
+print('Seu texto foi:\n {}'.format(frase))
+print('A letra "''A''" aparece {} vezes\nA letra "''A''" apareceu a 1ª vez na memória {}\nA última vez na memória {}'.format(frase.count('A'), frase.find('A')+1, frase.rfind('A')+1))
