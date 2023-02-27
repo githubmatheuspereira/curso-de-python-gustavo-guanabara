a75bf30e4f88f83e908f902b0e0c5e630e1ed551
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-02-27 20:48:48 -0300

    adicionado cores a um exercicio

--- CursoGuanabaraPython/desafio14.py
+++ CursoGuanabaraPython/desafio14.py
@@ -1,3 +1,3 @@
 celsius = float(input('Qual a temperatura em celsius? '))
 fahrenheit = float((9 * celsius) / 5) + 32
-print ('A temperatura em celsius é {}°C e em Fahrenheit ficará {}°F'.format(celsius, fahrenheit))
+print ('A temperatura em celsius é \033[0;32m{}°C\033[m e em Fahrenheit ficará \033[0;35m{}°F\033[m'.format(celsius, fahrenheit))
