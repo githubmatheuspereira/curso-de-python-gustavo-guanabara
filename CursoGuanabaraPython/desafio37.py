848c6b6747f7e3a4a27a44040eedc7c1cd55581b
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-03-02 21:58:31 -0300

    adicionado desafio 37

---
+++ CursoGuanabaraPython/desafio37.py
@@ -0,0 +1,17 @@
+# MINHA SOLUÇÃO PARA O DESAFIO 37 PROPOSTO
+
+numero = int(input('Qual número inteiro você quer transformar?\n'))
+
+conversao = str(input('Para qual conversão você gostaria?\n[1] Binário\n[2] Octal\n[3] Hexadecimal\n'))
+
+if conversao == '1':
+    numeroConvertido = bin(numero)
+    print('O número {} em binário é {}'.format(numero, numeroConvertido))
+elif conversao == '2':
+    numeroConvertido = oct(numero)
+    print('O número {} em octal é {}'.format(numero, numeroConvertido))
+elif conversao == '3':
+    numeroConvertido = hex(numero)
+    print('O número {} em hexadecimal é {}'.format(numero, numeroConvertido))
+else:
+    print('ERROR - OPÇÃO NÃO ENCONTRADA!')
