94e45ec31e10940a4d8c770d194952e77cd8d426
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-03-08 19:05:06 -0300

    atualizado desafio 39
    atualizado desafio 39 com solução do Guanabara após aula.

--- CursoGuanabaraPython/desafio39.py
+++ CursoGuanabaraPython/desafio39.py
@@ -8,3 +8,26 @@ elif idade > 17 and idade < 19:
      print('Prazer, {}!\nJá é hora de se alistar. Não perca o tempo!'.format(nome))
 else:
      print('Prazer, {}!\nJá passsou da hora de você se alistar. Vá até uma base do exército o quanto antes.'.format(nome))
+
+"""
+Solução Guanabara
+
+from datetime import date
+atual = date.today().year
+nasc = int(input('Ano de nascimento: '))
+idade = atual - nasc
+print('Quem nasceu em {} tem {} em {}'.format(nasc, idade, atual))
+
+if idade == 18:
+     print('Você tem que se alistar IMEDIATAMENTE!')
+elif idade < 18:
+     saldo = 18 - idade
+     print('Você ainda não tem 18 anos. Faltam {} anos para o alistamento.'.format(saldo))
+     ano = atual + saldo
+     print('Seu alistamento será em {}.'.format(ano))
+elif idade > 18:
+     saldo = idade - 18
+     print('Você já deveria ter se alistado há {} anos.'.format(saldo))
+     ano = atual - saldo
+     print('Seu alistamento foi em {}.'.format(ano))
+"""
