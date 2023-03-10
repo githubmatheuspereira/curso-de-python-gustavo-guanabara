5e7d1b389e88e367f10b58089761d7b236b23c80
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-03-09 21:35:38 -0300

    adicionado resolução guanabara

--- CursoGuanabaraPython/desafio41.py
+++ CursoGuanabaraPython/desafio41.py
@@ -12,3 +12,22 @@ elif idade <= 20:
     print('{},\nDe acordo com sua idade({} anos), você é considerado um atleta SÊNIOR!'.format(nome, idade))
 else:
     print('{},\nDe acordo com sua idade({} anos), você é considerado um atleta MASTER!'.format(nome, idade))
+
+"""
+RESOLUÇÃO GUANABARA
+from datetime import date
+atual = date.today().year
+nascimento = int(input('Ano de Nascimento: '))
+idade = atual - nascimento
+print('O atleta tem {} anos.'.format(idade))
+if idade <= 9:
+    print('Classificação: MIRIM')
+elif idade <= 14:
+    print('Classificação: INFANTIL')
+elif idade <= 19:
+    print('Classificação: JUNIOR')
+elif idade <= 25:
+    print('Classificação: SÊNIOR')
+elif:
+    print('Classificação: MASTER')
+"""
