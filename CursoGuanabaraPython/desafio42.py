5def72f594babd44e7cbce3bfb88113462edacfe
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-03-11 15:03:11 -0300

    desafio 42 atualizado

--- CursoGuanabaraPython/desafio42.py
+++ CursoGuanabaraPython/desafio42.py
@@ -6,7 +6,7 @@ linha3 = float(input('Digite o comprimento da terceira reta: '))
 
 if linha1 < linha2 + linha3 and linha2 < linha1 + linha3 and linha3 < linha1 + linha2:
     print('Os valores acima podem formar um triângulo!')
-    if linha1 == linha2 and linha1 == linha3:
+    if linha1 == linha2 == linha3:
         print('Este é um triângulo equilátero, todos os lados são iguais!')
     elif linha1 == linha2 or linha2 == linha3 or linha3 == linha1:
         print('Este é um triângulo isósceles, dois lados são iguais!')
