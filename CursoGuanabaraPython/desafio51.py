37442faff3df3f94599a5c4268fdbab9e0ad1fb2
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-03-12 19:04:01 -0300

    adicionado desafio 51

---
+++ CursoGuanabaraPython/desafio51.py
@@ -0,0 +1,8 @@
+# MINHA SOLUÇÃO PARA O DESAFIO 51 PROPOSTO
+
+termo = int(input('Qual é o primeiro termo?\n'))
+razao = int(input('Qual é a razão?\n'))
+print('-'*10,'Resutado abaixo','-'*10)
+for contador in range (0,10):
+    print(termo)
+    termo += razao
