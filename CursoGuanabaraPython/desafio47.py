a15fa7f5efd83f6786ce846d92c34a86838a7457
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-03-13 19:57:04 -0300

    atualizado desafio 47

--- CursoGuanabaraPython/desafio47.py
+++ CursoGuanabaraPython/desafio47.py
@@ -5,3 +5,11 @@ for contador in range(1,51):
     if contador % 2 == 0:
         print(contador)
         sleep(0.2)
+
+"""
+Solução Guanabara
+
+for n in range(2, 51, 2)
+    print(n, end=' ')
+print('Acabou!')
+"""
