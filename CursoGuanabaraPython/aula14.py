33cec154108bfd3fa1ea1666b2296dcc10bd28e6
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-03-22 21:02:29 -0300

    atualizado aula 14

--- CursoGuanabaraPython/aula14.py
+++ CursoGuanabaraPython/aula14.py
@@ -3,3 +3,15 @@ while resposta == 'S':
     valor = float(input('Digite um valor: '))
     resposta = str(input('Quer continuar?\n[S] ou [N] ')).upper()
 print('fim')
+
+numero = 1
+par = 0
+impar = 0
+while numero != 0:
+    numero = int(input('Digite um número: '))
+    if numero != 0:
+        if numero % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+print('Você dígitou {} números pares e {} números impares.'.format(par, impar))
