593158134f5c93b3c9ef3f9613b5b8f369ce029c
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-03-28 18:34:11 -0300

    adicionado desafio 64

---
+++ CursoGuanabaraPython/desafio64.py
@@ -0,0 +1,14 @@
+# MINHA SOLUÇÃO PARA O DESAFIO 64 PROPOSTO
+
+numero = 0
+contador = 0
+total = 0
+while numero != 999:
+    numero = int(input('Dígite aqui: '))
+    if numero == 999:
+        numero = 999
+    else:
+        total += numero
+        numero = 0
+    contador += 1
+print('Tivemos {} numeros digitados e o total deles é {}'.format(contador, total))
