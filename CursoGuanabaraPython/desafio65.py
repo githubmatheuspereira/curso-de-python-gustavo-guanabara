4a3ae78bfe3333b5e6c6578dd197a868205054dc
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-03-29 20:35:23 -0300

    adicionado desafio 65

---
+++ CursoGuanabaraPython/desafio65.py
@@ -0,0 +1,24 @@
+# MINHA SOLUÇÃO PARA O DESAFIO 65 PROPOSTO
+
+numero = 0
+total = 0
+media = 0
+maior = 0
+menor = 0
+continuar = ''
+contador = 0
+
+while continuar != 'N':
+    numero = int(input('Dígite um número: '))
+    continuar = ''
+    total += numero
+    if numero == 0 or numero > maior:
+        maior = numero
+    if menor == 0 or numero < menor:
+        menor = numero
+    while (continuar != 'S') and (continuar != 'N'):
+        continuar = str(input('Quer continuar? [S] SIM [N] NÃO\n')).upper()
+        if continuar != 'S' and continuar != 'N':
+            print('Comando errado, tente novamente!')
+print(menor, maior, total)
+        
