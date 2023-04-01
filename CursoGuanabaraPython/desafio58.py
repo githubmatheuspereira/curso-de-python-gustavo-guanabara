123d70a26edbc379bef3188f0ca16bb19840d480
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-04-01 18:52:50 -0300

    atualizado desafio 58
    Atualizado com resolução do Guanabara utilizando booleano

--- CursoGuanabaraPython/desafio58.py
+++ CursoGuanabaraPython/desafio58.py
@@ -13,3 +13,25 @@ while numeroEscolhido != numeroSorteado:
         chances += 1
         print('Você acertou o número é {}. Você precisou de {} chances.'.format(numeroSorteado, chances))
 print('Volte sempre!')
+
+#SOLUÇÃO GUANABARA
+"""
+
+from random import randint
+computador = randint(0, 10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.\nSerá que você consegue adivinhar qual foi? ')
+acertor = False
+palpite = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpite += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... Tente mais uma vez.')
+        elif jogador > computador:
+            print('Menos... Tente mais uma vez.')
+print('Acertou com {} tentativas. Parabéns!'.format(palpite))
+
+"""
