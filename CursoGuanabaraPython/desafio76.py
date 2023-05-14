30590586a7bf5ec78e7541d225a18145c69773aa
Matheus Pereira Silva <103319127+githubmatheuspereira@users.noreply.github.com>
2023-05-14 11:16:28 -0300

    adicionado desafio 76

---
+++ CursoGuanabaraPython/desafio76.py
@@ -0,0 +1,7 @@
+# MINHA SOLUÇÃO PARA O DESAFIO 76 PROPOSTO
+print(':'*50,f'\n{"LISTA DE COMPRAS":.^50}')
+print(':'*50)
+lista = ('Arroz', 21.30, 'Feijão', 9.40, 'Óleo', 8.90, 'Sal', 4.75, 'Açúcar', 3.80, 'Café', 19.70, 'Extrato de tomate', 4.99, 'Macarrão', 3.25, 'Atum', 7.90, 'Milho/Ervilha', 2.90, 'Farinha de trigo', 6.40, 'Manteiga', 12.65)
+for produto in range(0, len(lista), 2):
+    print(f'{lista[produto]:_<40}',f'R$ {lista[produto+1]:>5.2f}')
+print('_'*50,f'\n{"Todos direitos reservados":_^50}\n')
